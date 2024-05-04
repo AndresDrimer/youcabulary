faaee1de2746e6dfd10c4921f41193e0239ae16a
AndresDrimer <andresdrimer@hotmail.com>
2024-05-04 14:07:12 -0300

    recoverd bin

--- src/views/home.php
+++ src/views/home.php
@@ -142,9 +142,10 @@ $voiceCountry = $_SESSION["voiceCountry"];
             <form action="src/controllers/del_word_control.php" method="post" class="bin-complete-form">
                 <input type="hidden" name="uuid" value="<?php echo $user_uuid; ?>">
                 <input type="hidden" name="delete_word" value="<?php echo $word['str']; ?>">
-                <button type="submit" value="" id="del-word-btn"><span class="material-symbols-outlined">
-                        delete
-                    </span></button>
+                <button type="submit" value="" id="del-word-btn">
+
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                </button>
             </form>
 
 <!-- esto no esta, es donde irian las banderas de cada locucion
